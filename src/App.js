fe7b4c264dfca736f93e9caafe5d42b8aca626a7
Arpit <arpit2911011@gmail.com>
2023-01-20 15:13:57 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+// import DirectMessaging from './DirectMessaging';
+import './App.css';
+import {ChatEngine} from 'react-chat-engine';
+
+const projectID = 'db5b8d4a-51dd-4845-9917-9b6df45c7f48';
+
+//Using arrow function component
+const App = () => {
+
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
